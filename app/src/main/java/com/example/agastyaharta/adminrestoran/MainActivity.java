0b7ef705ac3f7316bdd550195b1585478dce380a
agastyaharta <agastya.harta98@gmail.com>
2018-11-30 13:15:17 +0800

    tadi failed skrg coba push lagi

--- app/src/main/java/com/example/agastyaharta/adminrestoran/MainActivity.java
+++ app/src/main/java/com/example/agastyaharta/adminrestoran/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.agastyaharta.adminrestoran;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String URL ="http://0.0.0.0:8000";
+
+    GridLayout mainGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainGrid = (GridLayout)findViewById(R.id.mainGrid);
+
+        setSingleEvent(mainGrid);
+
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        for (int i=0; i < mainGrid.getChildCount(); i++) {
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final  int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (finalI == 0) {
+                        Intent intent = new Intent(MainActivity.this, AddMenuActivity.class);
+                        startActivity(intent);
+                    } else if (finalI == 1) {
+                        Intent intent = new Intent(MainActivity.this, EditMenuActivity.class);
+                        startActivity(intent);
+                    } else if (finalI == 2) {
+                        Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                        startActivity(intent);
+                    } else if (finalI == 3) {
+                        Intent intent = new Intent(MainActivity.this, FixedMenuActivity.class);
+                        startActivity(intent);
+                    }
+
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/agastyaharta/adminrestoran/SplashActivity.java
+++ app/src/main/java/com/example/agastyaharta/adminrestoran/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.example.agastyaharta.adminrestoran;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.agastyaharta.adminrestoran.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
