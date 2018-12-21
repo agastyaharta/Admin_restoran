bde8fdd669bd0a15d7181b1bca23c39b44f03cc0
agastyaharta <agastya.harta98@gmail.com>
2018-12-21 16:47:23 +0800

    dah mau lese

---
+++ app/src/main/java/com/example/agastyaharta/adminrestoran/activity/EditMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.agastyaharta.adminrestoran.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_menu);
+    }
+}
---
+++ app/src/main/java/com/example/agastyaharta/adminrestoran/activity/InsertMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.agastyaharta.adminrestoran.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InserMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inser_menu);
+    }
+}
